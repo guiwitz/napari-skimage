af9016e8264eb30319bf5078632cdc1ab3ba9606
guiwitz <guiwitz@users.noreply.github.com>
2024-05-24 01:55:35 +0200

    morphology operations

--- src/napari_skimage/__init__.py
+++ src/napari_skimage/__init__.py
@@ -6,9 +6,12 @@ except ImportError:
     __version__ = "unknown"
 from .skimage_filter_widget import farid_filter_widget, gaussian_filter_widget
 from .skimage_threshold_widget import threshold_widget
+from .skimage_morphology_widget import connected_components_widget, binary_morphology_widget
 
 __all__ = (
     "farid_filter_widget",
     "gaussian_filter_widget",
     "threshold_widget",
+    "connected_components_widget",
+    "binary_morphology_widget",
 )
---
+++ src/napari_skimage/skimage_morphology_widget.py
@@ -0,0 +1,42 @@
+from typing import TYPE_CHECKING
+
+from magicgui import magic_factory
+import skimage.morphology as sm
+from napari.layers import Image, Labels
+import napari.types
+
+
+if TYPE_CHECKING:
+    import napari
+
+@magic_factory(
+    label_layer={'label': 'Labels'},
+ )
+def connected_components_widget(
+    label_layer: Labels) -> napari.types.LayerDataTuple:
+    return (
+        sm.label(label_layer.data),
+        {'name': f'{label_layer.name}_labels'},
+        'labels')
+    return 
+@magic_factory(
+    label_layer={'label': 'Labels'},
+    method={'choices': ['erosion', 'dilation', 'opening', 'closing']},
+    footprint={'label': 'Footprint', 'choices': ['disk', 'square', 'diamond', 'star', 'octagon']},
+    footprint_size={'label': 'Footprint size', 'max': 100, 'min': 1, 'step': 1},
+    call_button="Apply operation"
+)
+def binary_morphology_widget(
+    label_layer: Labels,
+    method = "erosion",
+    footprint = "disk",
+    footprint_size = 3,
+) -> napari.types.LayerDataTuple:
+    fun = getattr(sm, f'binary_{method}')
+    fun_footprint = getattr(sm, footprint)
+    selem = fun_footprint(footprint_size)
+    mask = fun(label_layer.data, selem)
+    return (
+        mask,
+        {'name': f'{label_layer.name}_{method}'},
+        'labels')
