e7ad383c24968f4a7b6d4edd44a016a6db1a2783
guiwitz <guiwitz@users.noreply.github.com>
2024-05-29 11:16:22 +0200

    update init

--- src/napari_skimage/__init__.py
+++ src/napari_skimage/__init__.py
@@ -4,14 +4,32 @@ try:
     from ._version import version as __version__
 except ImportError:
     __version__ = "unknown"
-from .skimage_filter_widget import farid_filter_widget, gaussian_filter_widget
+from .skimage_filter_widget import (farid_filter_widget, prewitt_filter_widget,
+                                    laplace_filter_widget, gaussian_filter_widget,
+                                    frangi_filter_widget, median_filter_widget,
+                                    butterworth_filter_widget)
 from .skimage_threshold_widget import threshold_widget
-from .skimage_morphology_widget import connected_components_widget, binary_morphology_widget
+from .skimage_morphology_widget import (connected_components_widget,
+                                        binary_morphology_widget, morphology_widget)
+from .skimage_restoration_widget import (rolling_ball_restoration_widget,
+                                         denoise_nl_means_restoration_widget)
+from. mathsops import (simple_maths_widget, maths_image_pairs_widget, conversion_widget)
 
 __all__ = (
     "farid_filter_widget",
+    "prewitt_filter_widget",
+    "laplace_filter_widget",
     "gaussian_filter_widget",
+    "frangi_filter_widget",
+    "median_filter_widget",
+    "butterworth_filter_widget",
     "threshold_widget",
     "connected_components_widget",
     "binary_morphology_widget",
+    "morphology_widget",
+    "rolling_ball_restoration_widget",
+    "denoise_nl_means_restoration_widget",
+    "simple_maths_widget",
+    "maths_image_pairs_widget",
+    "conversion_widget",
 )
