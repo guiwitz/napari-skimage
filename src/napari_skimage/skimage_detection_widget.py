3fa9fabae48c2e0b99f9c86ee9b2f8f198541b81
Johannes Müller <38459088+jo-mueller@users.noreply.github.com>
2024-08-07 14:12:35 +0200

    added _init functions for links to skimage reference

--- src/napari_skimage/skimage_detection_widget.py
+++ src/napari_skimage/skimage_detection_widget.py
@@ -1,6 +1,8 @@
 from typing import TYPE_CHECKING
 import numpy as np
 from magicgui import magic_factory
+from qtpy.QtCore import Qt
+from magicgui.widgets import Label
 from skimage.feature import peak_local_max
 from skimage.measure import marching_cubes
 from napari.layers import Image, Labels
@@ -9,18 +11,40 @@ import napari.types
 if TYPE_CHECKING:
     import napari
 
+
+def _on_init_peak_local_max(widget):
+    label_widget = Label(value='')
+    func_name = '_'.join(widget.label.split(' ')[:-1])
+    label_widget.value = f'<a href=\"https://scikit-image.org/docs/stable/api/skimage.feature.html#skimage.feature.{func_name}\">skimage.feature.{func_name}</a>'
+    label_widget.native.setTextFormat(Qt.RichText)
+    label_widget.native.setTextInteractionFlags(Qt.TextBrowserInteraction)
+    label_widget.native.setOpenExternalLinks(True)
+    widget.extend([label_widget])
+
+
+def _on_init_marching_cubes(widget):
+    label_widget = Label(value='')
+    func_name = '_'.join(widget.label.split(' ')[:-1])
+    label_widget.value = f'<a href=\"https://scikit-image.org/docs/stable/api/skimage.measure.html#skimage.measure.{func_name}\">skimage.measure.{func_name}</a>'
+    label_widget.native.setTextFormat(Qt.RichText)
+    label_widget.native.setTextInteractionFlags(Qt.TextBrowserInteraction)
+    label_widget.native.setOpenExternalLinks(True)
+    widget.extend([label_widget])
+
+
 @magic_factory(
     image_layer={'label': 'Image'},
     min_distance={'label': 'Minimum Distance'},
     threshold_absolute={'label': 'Threshold Absolute', 'min': 0.0, 'max': 65535},
     threshold_relative={'label': 'Threshold Relative', 'min': 0.0, 'max': 1.0},
-    call_button="Detect Local Maxima"
+    call_button="Detect Local Maxima",
+    widget_init=_on_init_peak_local_max
 )
 def peak_local_max_widget(
     image_layer: Image,
     min_distance: int = 10,
     threshold_absolute: float = 0,
-    threshold_relative: float = 0.0
+    threshold_relative: float = 0.0,
 ) -> napari.types.LayerDataTuple:
     coordinates = peak_local_max(image_layer.data,
                                  min_distance=min_distance,
@@ -32,11 +56,13 @@ def peak_local_max_widget(
         'points'
     )
 
+
 @magic_factory(
     label_layer={'label': 'Labels'},
     label={'label': 'Label'},
     binarize={'label': 'Binarize'},
-    call_button="Apply Marching Cubes"
+    call_button="Apply Marching Cubes",
+    widget_init=_on_init_marching_cubes
 )
 def marching_cubes_widget(
     label_layer: Labels,
